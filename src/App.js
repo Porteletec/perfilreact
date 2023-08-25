5ffc84e30ded8f27f7ecdd544e25f2110a0dfadc
Porteletec <143223222+Porteletec@users.noreply.github.com>
2023-08-25 18:12:08 -0300

    inicio do projeto react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Kauã Portel Ferraresso</h1>
+      <p>Tenho 16 anos;</p>
+      <p>Estudo na Etec Polivalente em Americana (SP), cursnado Desenvolvimento de Sistemas;</p> 
+      <p>Nasci na cidade de Campinas (SP), mas moro na cidade de Sumaré (SP); </p>
+      <p>Gosto de esportes, músicas, livros, filmes, séries,carros, motos, dentre outras coisas;</p>
+    </div>
+  );
+}
